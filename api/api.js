ccfebfdd30e16e033e15963deb76b2a4fee04ae6
zakhar bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2023-11-23 15:32:31 +0100

    get any of nuts

--- api/api.js
+++ api/api.js
@@ -3,4 +3,12 @@ const router = express.Router();
 const orzechy = require("../orzechy");
 const joi = require("joi");
 
+router.get("/orzechy", (req, res) => {
+    res.json(orzechy.list());
+})
+
+router.get("/orzechy/:id", (req, res) => {
+    res.json(orzechy.get(req.params.id));
+})
+
 module.exports = router;
--- orzechy/orzechy.js
+++ orzechy/orzechy.js
@@ -2,3 +2,21 @@ const fs = require("fs");
 const path = require("path");
 const orzechy = require("./orzechyDB");
 
+function copyOrzechy (obj) {
+    return JSON.parse(JSON.stringify(obj))
+}
+
+function listOrzechy () {
+    return copyOrzechy(orzechy);
+}
+
+function getOrzech (idOrzech) {
+    let id = +idOrzech;
+    let orzech = orzechy.find(o => o.id === id);
+    return copyOrzechy(orzech);
+}
+
+module.exports = {
+    list: listOrzechy,
+    get: getOrzech
+}
