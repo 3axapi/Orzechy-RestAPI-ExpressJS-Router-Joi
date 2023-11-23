e8a5c481e2a738cbae1705081e69f1ac56dd581a
zakhar bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2023-11-23 15:52:49 +0100

    save

--- orzechy/orzechy.js
+++ orzechy/orzechy.js
@@ -2,21 +2,38 @@ const fs = require("fs");
 const path = require("path");
 const orzechy = require("./orzechyDB");
 
-function copyOrzechy (obj) {
-    return JSON.parse(JSON.stringify(obj))
-}
-
 function listOrzechy () {
-    return copyOrzechy(orzechy);
-}
+    return parseOrzech(orzechy);
+} // dostać wszyskie orzechy
 
 function getOrzech (idOrzech) {
     let id = +idOrzech;
     let orzech = orzechy.find(o => o.id === id);
-    return copyOrzechy(orzech);
+    return parseOrzech(orzech);
+} // dostać konkretny orzech ze wskazanym id
+
+function addOrzech () {
+    
 }
+
+function deleteOrzech (idOrzech) {}
+
+function parseOrzech (obj) {
+    return JSON.parse(JSON.stringify(obj));
+} // dostać object z jsona
+
+function saveOrzechy () {
+    fs.writeFile (
+        path.join(__dirname, "orzechyBD.json"),
+        JSON.stringify(orzechy, null, "\t"),
+        err => {
+            if (err) console.log("Błąd podczas zapisywania danych");
+            else console.log("Plik został zapisany");
+        }
+    );
+} // zapisać zmiany w json
 
 module.exports = {
     list: listOrzechy,
     get: getOrzech
-}
+};
